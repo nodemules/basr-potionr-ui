54abcd2922a9ffce079c17da5239c355a8163a36
brenthaertlein <brenthaertlein@gmail.com>
2017-12-14 00:29:59 -0500

    master
    * progress

--- src/main/components/potion/PotionBrewer.js
+++ src/main/components/potion/PotionBrewer.js
@@ -1,12 +1,67 @@
 import React, {Component} from "react";
-import {Alert} from "reactstrap";
+import {Alert, Button, Progress} from "reactstrap";
+import "./PotionBrewer.css";
 
 class PotionBrewer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.brewPotion = this.brewPotion.bind(this);
+  }
+
+  brewPotion() {
+    this.setState({
+      progress: 0
+    });
+    let id;
+    let brew = () => {
+      this.setState({
+        progress: Math.ceil(this.state.progress + Math.random() * 4)
+      });
+      if (this.state.progress >= 100) {
+        clearInterval(id);
+      }
+    };
+    id = setInterval(brew, 1000);
+  }
+
   render() {
     let type = this.props.type;
+    let brewing = this.state.progress > 0 && this.state.progress < 100;
+    let completed = this.state.progress >= 100;
     return (
-        <div>
-          <Alert color={type.color}>{type.name}</Alert>
+        <div className="PotionBrewer">
+          <h2>Brewing {type.name} potions!</h2>
+          <Button id={type.name} color={type.color}
+                  onClick={this.brewPotion}
+                  disabled={brewing}>
+            {
+              brewing ? 'Brewing...' : 'Brew'
+            }
+          </Button>
+          {
+            completed ? //
+                <Alert color={type.color}>
+                  {type.name} potion completed brewing!
+                </Alert>
+                : null
+          }
+          {
+            this.state.progress <= 100 ? //
+                <Progress className="PotionBrewer-progress"
+                          animated
+                          striped
+                          value={this.state.progress}
+                          color={type.color}>
+                  <span className="PotionBrewer-progress-text">
+                  {this.state.progress} %
+                  </span>
+                </Progress>
+                : null
+          }
+
         </div>
     )
   }
